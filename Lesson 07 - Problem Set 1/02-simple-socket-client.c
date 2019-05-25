b12cebd70d4c9a798950ba925247ac5c112e4cb2
Steven <stevenxchung@gmail.com>
2019-05-25 12:13:21 -0400

    Create 02-simple-socket-client.c

---
+++ Lesson 07 - Problem Set 1/02-simple-socket-client.c
@@ -0,0 +1,22 @@
+// Simple socket client
+// Pseudo code
+
+// Include packages
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <netinet/in.h>
+
+// Constants
+#define SERVER_NAME "localhost"
+#define PORT 8000
+
+// Connect to server socket if available
+int main() {
+    // Set localhost
+    // Create socket
+    // Connect to server if exists
+    // Close socket
+}
