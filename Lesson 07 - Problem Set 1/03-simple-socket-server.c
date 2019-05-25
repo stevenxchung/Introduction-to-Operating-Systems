42e53ace8197e2774968a6675f8d527c2b0c8c01
Steven <stevenxchung@gmail.com>
2019-05-25 12:13:24 -0400

    Create 03-simple-socket-server.c

---
+++ Lesson 07 - Problem Set 1/03-simple-socket-server.c
@@ -0,0 +1,20 @@
+// Simple socket server
+// Pseudo code
+
+// Include packages
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <unistd.h>
+#include <netdb.h>
+
+// Constants
+#define PORT 8000
+
+// Accept a client socket if available
+int main() {
+    // Create socket
+    // Listen on socket
+    // Accept client
+    // Close socket
+}
