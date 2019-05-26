213a442edfa13c0e78d6e5d4dc08b859a3a0a873
Steven <stevenxchung@gmail.com>
2019-05-26 09:30:31 -0400

    Create 04-the-echo-protocol-client.c

---
+++ Lesson 07 - Problem Set 1/04-the-echo-protocol-client.c
@@ -0,0 +1,24 @@
+// The echo protocol client
+// Pseudo code
+
+// Include packages
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <netinet/in.h>
+
+// Constants
+#define SERVER_NAME "localhost"
+#define PORT 8000
+#define MESSAGE "hello world!"
+
+// Connect to server socket if available and send message
+int main() {
+    // Set localhost
+    // Create socket
+    // Connect to server if exists
+    // Send message
+    // Close socket
+}
