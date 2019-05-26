4b34a2822f195794cc7941ae6b1950533efc6a94
Steven <stevenxchung@gmail.com>
2019-05-26 09:30:33 -0400

    Create 04-the-echo-protocol-server.c

---
+++ Lesson 07 - Problem Set 1/04-the-echo-protocol-server.c
@@ -0,0 +1,26 @@
+// The echo protocol server
+// Pseudo code
+
+// Include packages
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <netinet/in.h> 
+
+// Constants
+#define PORT 8000
+
+// Accept a client socket if available, read message, modify message, and reply
+int main() {
+    // Create socket
+    // Listen on socket
+    // Accept client
+    // Read and modify message
+    // Send message to client
+    // Close socket
+}
